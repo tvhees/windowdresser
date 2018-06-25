d531a3450810bbd5b835b67c97a85729455bce7c
Torbjorn van Heeswijck <t.vanheeswijck@gmail.com>
2018-06-25 22:45:31 +0930

    adding basic data and render function for screenshots

--- src/WindowDresser/dresser.js
+++ src/WindowDresser/dresser.js
@@ -12,13 +12,23 @@ const output = loadData('output');
 
 exports.sourcePath = type => localPath(config.src, source[type].file);
 
+const isLandscape = image => image.bitmap.width >= image.bitmap.height;
+
+const dimensions = (width, height, src) => {
+  if (isLandscape(src) !== (height > width)) { // Effectively an XOR gate
+    return [width, height];
+  }
+
+  return [height, width];
+};
+
 const renderFunction = {
-  cover: image => (width, height, name) => {
-    image.clone().cover(width, height).write(localPath(config.dest, name));
-  },
-  icon: image => (width, height, name) => {
-    image.clone().resize(width, height).write(localPath(config.dest, name));
-  },
+  cover: image => (width, height, name) =>
+    image.clone().cover(width, height).write(localPath(config.dest, name)),
+  icon: image => (width, height, name) =>
+    image.clone().resize(width, height).write(localPath(config.dest, name)),
+  screens: image => (width, height, name) =>
+    image.clone().cover(...dimensions(width, height, image)).write(localPath(config.dest, name)),
 };
 
 const writeFromImage = (type, platforms) => image =>
